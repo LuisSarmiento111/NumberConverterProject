f1bbe0f886318d97dd5c5a2f5bb63ba21179b80b
BT_4N2_05 <lsarmiento9311@bths.edu>
2023-01-09 12:34:03 -0500

    Worked on method convertToDecimal to convert any base number to decimal Wrote method validNumberForBases to check to see if the number inserted follows a base's number system. Worked on the runner class to get user input through scanner.

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -1,15 +1,15 @@
 public class BaseConverter {
     private String[] digits;
-    private int number;
+    private int base;
 
-    public BaseConverter(int number) {
+    public BaseConverter(int base) {
          digits = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
             "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e",
             "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "+", "/"};
-        this.number = number;
+         this.base = base;
     }
 
-    public String convert(int base) {
+    public String convert(int number, int base) {
         String newNum = "";
         while (number != 0) {
             newNum = (digits[number % base]) + newNum;
@@ -18,12 +18,31 @@ public class BaseConverter {
         return newNum;
     }
 
-    public String convertToDecimal(int number) {
-        String newNum = "";
+    public int convertToDecimal(int number, int base) {
+        int newNum = 0;
+        String numberAsString = number + "";
+        for (int i = numberAsString.length() - 1; i >= 0; i--) {
+            newNum += (Integer.parseInt(numberAsString.substring(i, i + 1)) * (int) Math.pow(base, numberAsString.length() - (1 + i)));
+        }
+        return newNum;
+    }
+
+    public boolean validNumberForBase(String number){
+        boolean valid = false;
+        for (int i = base; i < digits.length; i++) {
+            if (number.contains(digits[i])) {
+                valid = true;
+            }
+        }
+        return valid;
+    }
+
+    public int convertBtoD(int number){
+        return convertToDecimal(number, 10);
     }
 
     public String getDigits() {
-        return digits[35];
+        return digits[31];
     }
 }
 
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,36 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 public class Test {
     public static void main(String[] args)
     {
-        BaseConverter test = new BaseConverter(6252);
-        System.out.print(test.convert(64));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Number Converter!\n--------------------------------");
+        System.out.println("Would you like to use the (b)asic converter or the (a)dvanced converter? (b or a)");
+        String answer = scanner.nextLine();
+        while (answer.equals("b") == false && answer.equals("a") == false) {
+            System.out.println("That is not a valid answer");
+            System.out.println("Would you like to use the (b)asic converter or the (a)dvanced converter? (b or a)");
+            answer = scanner.nextLine();
+        }
+        if (answer.equals("b")) {
+            System.out.println("Enter the base of your number (2, 8 or 10): ");
+            answer = scanner.nextLine();
+            while (answer.equals("2") == false && answer.equals("8") == false && answer.equals("10") == false){
+                System.out.println("That is not a valid base value for the basic converter");
+                System.out.println("Enter the base of your number (2, 8 or 10): ");
+                answer = scanner.nextLine();
+            }
+            int base = Integer.parseInt(answer);
+            BaseConverter converter = new BaseConverter(base);
+            System.out.println("Enter your number: ");
+            answer = scanner.nextLine();
+            while (converter.validNumberForBase(answer)){
+                System.out.println("There is a number/letter that does not comply with your base's number system.");
+                System.out.println("Enter your number: ");
+                answer = scanner.nextLine();
+            }
+            int number = Integer.parseInt(answer);
+            System.out.println(converter.convertBtoD(Integer.parseInt()));
+        }
     }
 }
